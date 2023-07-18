fb35d736731efd8ce3cc49f9c53d380e9535de1b
7jjin <wlsgud6305@naver.com>
2023-07-19 00:46:54 +0900

    슬라이드 나오는것 까지

--- practice/teamproject_section3.js
+++ practice/teamproject_section3.js
@@ -1,28 +1,44 @@
-const slide = document.querySelector(".img_content");
-const slideImg = document.querySelectorAll(".img_content li");
-let currentIndex = 0;
-const slideCount = slideImg.length;
-const prev = document.querySelector(".button_before");
-const next = document.querySelector(".button_next");
-const slidWidth = 400;
+var slideIndex = 1;
+var slides = document.querySelectorAll(".img_content li");
+var prevBtn = document.querySelector(".button_before");
+var nextBtn = document.querySelector(".button_next");
 
-slide.style.width = slidWidth * slideCount + "px";
-console.log(slidWidth * slideCount + "px");
-function moveSilde(num) {
-  slide.style.width = -num * 400 + "px";
-  console.log(slide.style.width);
-  currentIndex = num;
-}
+showSlide(slideIndex);
 
-prev.addEventListener("click", function () {
-  console.log("a");
-  if (currentIndex !== 0) moveSilde(currentIndex - 1);
+prevBtn.addEventListener("click", function () {
+  slideIndex--;
+  showSlide(slideIndex);
 });
 
-next.addEventListener("click", function () {
-  if (currentIndex !== slideCount - 1) {
-    console.log("b");
-    console.log(currentIndex);
-    moveSilde(currentIndex + 1);
+nextBtn.addEventListener("click", function () {
+  slideIndex++;
+  showSlide(slideIndex);
+});
+
+function showSlide(index) {
+  if (index < 0) {
+    slideIndex = slides.length - 1;
+  } else if (index >= slides.length) {
+    slideIndex = 0;
   }
-});
+
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  // 현재 슬라이드, 이전 슬라이드, 다음 슬라이드만 보이도록 설정
+  slides[slideIndex].style.display = "flex";
+  slides[slideIndex].style.flexDirection = "column";
+  slides[slideIndex].style.transform = "rotate(0deg)";
+
+  var prevIndex = slideIndex - 1 < 0 ? slides.length - 1 : slideIndex - 1;
+  slides[prevIndex].style.display = "flex";
+  slides[prevIndex].style.transform = "rotate(-15deg)";
+  slides[prevIndex].style.flexDirection = "column";
+
+  var nextIndex = slideIndex + 1 >= slides.length ? 0 : slideIndex + 1;
+  slides[nextIndex].style.display = "flex";
+  slides[nextIndex].style.transform = "rotate(15deg)";
+
+  slides[nextIndex].style.flexDirection = "column";
+}
