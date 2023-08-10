fb28161e66310b6ee437421217ff7fbe8b600a1a
7jjin <wlsgud6305@naver.com>
2023-08-10 20:51:14 +0900

    backend project

---
+++ backend_project/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const PORT = 8000;
+const db = require("./models");
+
+app.set("view engine", "ejs");
+// app.set("views", "./views"); // NodeJS는 기본적으로 views포트를 참고하고 있어서 생략 가능
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+const studentRouter = require("./routes/student");
+app.use("/student", studentRouter);
+
+db.sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+  });
+});
