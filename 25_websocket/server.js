5dce84a315fc116184766400ec008e392bccbe94
7jjin <wlsgud6305@naver.com>
2023-08-29 14:48:41 +0900

    더 좋은 방법

--- 25_websocket/server.js
+++ 25_websocket/server.js
@@ -1,6 +1,12 @@
+const http = require("http");
 const ws = require("ws");
 const express = require("express");
 const app = express();
+//http서버
+const server = http.createServer(app);
+// 웹 소켓 서버 접속
+const wss = new ws.Server({ server });
+
 const PORT = 8000;
 
 app.set("view engine", "ejs");
@@ -10,13 +16,7 @@ app.get("/", (req, res) => {
   res.render("client");
 });
 
-const server = app.listen(PORT, () => {
-  console.log(`http://localhost:${PORT}`);
-});
-
 // app.listen 아래에 써도 된다.
-// 웹 소켓 서버 접속
-const wss = new ws.Server({ server });
 
 // 브라우저(클라이언트)들을 담을 배열변수
 const sockets = [];
@@ -24,11 +24,21 @@ const sockets = [];
 wss.on("connection", (socket) => {
   console.log("클라이언트가 연결되었습니다.");
 
+  //클라이언트 상태확인
+  //ws.CONNECETING : 0 웹소켓이 연결 시도중
+  //ws.OPEN : 1 윕소켓이 열린상태
+  //ws.CLOSING : 2 웹소켓이 닫히는 중
+  //ws.CLOSED : 3 웹소켓이 닫힌상태
+  //socket.readyState : 4 웹소켓의 클라이언트 상태를 나타내는 속성
+  console.log(ws.OPEN);
+
   //sockets 배열에 브라우저 배열 추가
   sockets.push(socket);
 
   // 메세지 이벤트
   socket.on("message", (message) => {
+    // 소켓을 통해 클라이언트와 서버간의 데이터를 주고받을때는 일반적으로 문자열 또는 버퍼형태로 전달됨.
+    //서버가 모두 다른환경이기때문에 객체를 전달할 때는 객체를 일련의 바이트로 변환하는 직렬화과정이 필요 => 버퍼
     console.log(`클라이언트로부터 받은 메세지: ${message}`);
     let data = JSON.parse(message);
     // socket.send(`서버메세지:${message}`);
@@ -44,4 +54,8 @@ wss.on("connection", (socket) => {
   socket.on("close", () => {
     console.log("클라이언트와 연결이 종료");
   });
+});
+
+server.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
 });
